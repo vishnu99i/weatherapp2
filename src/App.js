1a1d165481a98d8f10fd6895eeb9410ad3ad9b2c
Vishnu P <vinventor1999@gmail.com>
2024-04-12 16:15:35 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React J
+          Learn React JS
         </a>
       </header>
     </div>
